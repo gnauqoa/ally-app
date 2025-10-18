8e800eb3b1d52a7ac9d6edbcde0b4c5136524cb3
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2025-10-18 09:10:12 +0700

    init shadcn/ui

---
+++ src/pages/chat/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const ChartScreen = () => {
+  return <Button>Click me</Button>;
+};
+
+const Message = () => {
+
+}
+
+export default ChartScreen;
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
